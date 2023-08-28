7d667f26136a3848ce6e6d6fba40ae55cdd2d40c
amb258012@gmail.com <amb258012@gmail.com>
2023-08-28 22:43:07 +0900

    프로젝트 세팅 , 프론트페이지 템플릿 설치

---
+++ src/main/java/com/hoyo/kcharity/KcharityApplication.java
@@ -0,0 +1,18 @@
+package com.hoyo.kcharity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcharityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KcharityApplication.class, args);
+	}
+
+
+
+}
+
+
+
---
+++ src/test/java/com/hoyo/kcharity/KcharityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoyo.kcharity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcharityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
